2e4de1d583be968d9a4fdee76492137a3feb5e9d
Nelmeris <kufaevartem@icloud.com>
2020-04-01 23:22:56 +0300

    Доработка и исправление AuthService

--- client/src/app/services/auth.service.ts
+++ client/src/app/services/auth.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import {User} from '../models/user.model';
+import {DataBaseService} from './database.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export class AuthService {
 
   authStorageKey = 'user_auth_id';
 
-  constructor() { }
-  isAuth() {
+  constructor(private dataBaseService: DataBaseService) { }
+  isAuth(): boolean {
     return window.localStorage.getItem(this.authStorageKey) != null;
   }
   setAuth(user: User) {
     window.localStorage.setItem(this.authStorageKey, user.id.toString());
+  }
+  async getAuthUser(): Promise<User> {
+    const userId = window.localStorage.getItem(this.authStorageKey);
+    return await this.dataBaseService.getUserById(+userId);
   }
 }
