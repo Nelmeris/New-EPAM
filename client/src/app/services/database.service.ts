bbbbf7dfb1b907bb0e3a2fe29c416cfbdde0a8dc
Nelmeris <kufaevartem@icloud.com>
2020-04-01 22:29:09 +0300

    Сделаны get'ы из БД

--- client/src/app/services/database.service.ts
+++ client/src/app/services/database.service.ts
@@ -2,6 +2,12 @@ import { Injectable } from '@angular/core';
 import {BaseApi} from './base-api';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {User} from '../models/user.model';
+import {Rule} from '../models/rule.model';
+import {Order} from '../models/order.model';
+import {UserType} from '../models/user-type.model';
+import {OrderStatus} from '../models/order-status.model';
+import {OrderType} from '../models/order-type.model';
+import {UserTypeRule} from '../models/user-type-rule.model';
 
 @Injectable({
   providedIn: 'root'
@@ -17,11 +23,26 @@ export class DataBaseService extends BaseApi {
   }
 
   async getUsers() {
-    return this.get('users', this.options).toPromise();
+    const jsonArray = await this.get('users', this.options).toPromise();
+    const users: User[] = [];
+    for (const json of jsonArray) {
+      const user: User = new User(json);
+      user.phoneNumber = json.phone_number;
+      user.creationDate = json.creation_date;
+      user.typeId = json.user_type_id;
+      users.push(user);
+    }
+    return users;
   }
 
   async getOrders() {
-    return this.get('orders', this.options).toPromise();
+    const jsonArray = await this.get('orders', this.options).toPromise();
+    const objects: Order[] = [];
+    for (const json of jsonArray) {
+      const object = new Order(json);
+      objects.push(object);
+    }
+    return objects;
   }
 
   async getOrder(user: User) {
@@ -29,29 +50,57 @@ export class DataBaseService extends BaseApi {
   }
 
   async getUserTypes() {
-    return this.get('user_types', this.options).toPromise();
+    const jsonArray = await this.get('user_types', this.options).toPromise();
+    const objects: UserType[] = [];
+    for (const json of jsonArray) {
+      const object = new UserType(json);
+      objects.push(object);
+    }
+    return objects;
   }
 
   async getOrderTypes() {
-    return this.get('order_types', this.options).toPromise();
+    const jsonArray = await this.get('order_types', this.options).toPromise();
+    const objects: OrderType[] = [];
+    for (const json of jsonArray) {
+      const object = new OrderType(json);
+      objects.push(object);
+    }
+    return objects;
   }
 
   async getOrderStatuses() {
-    return this.get('order_statuses', this.options).toPromise();
+    const jsonArray = await this.get('order_statuses', this.options).toPromise();
+    const objects: OrderStatus[] = [];
+    for (const json of jsonArray) {
+      const object = new OrderStatus(json);
+      objects.push(object);
+    }
+    return objects;
   }
 
-  // async getPhones() {
-  //   return this.get('phones', this.options).toPromise();
-  // }
-  //
-  // async getPhoneById(id: number) {
-  //   return this.get('phones/' + id, this.options).toPromise();
-  // }
-  //
+  async getRules() {
+    const jsonArray = await this.get('rules', this.options).toPromise();
+    const rules: Rule[] = [];
+    for (const json of jsonArray) {
+      const rule = new Rule(json);
+      rules.push(rule);
+    }
+    return rules;
+  }
+
+  async getUserTypeRules() {
+    const jsonArray = await this.get('user_type_rules', this.options).toPromise();
+    const userTypeRules: UserTypeRule[] = [];
+    for (const json of jsonArray) {
+      const rule = new UserTypeRule(json);
+      userTypeRules.push(rule);
+    }
+    return userTypeRules;
+  }
   // async createPhone(phone) {
   //   return this.post('phones', JSON.stringify(phone), this.options).toPromise();
   // }
-  //
   // async editPhone(phone: Phone) {
   //   return this.put('phones/' + phone.id, JSON.stringify(phone), this.options).toPromise();
   // }
