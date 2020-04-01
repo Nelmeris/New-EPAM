5f25d20b151163de4843bf3234c31d71e6b5d1df
Nelmeris <kufaevartem@icloud.com>
2020-04-01 22:29:24 +0300

    Отображение вытянутых данных

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 import {DataBaseService} from './services/database.service';
+import {UserTypeRule} from './models/user-type-rule.model';
+import {Rule} from './models/rule.model';
+import {Order} from './models/order.model';
+import {OrderType} from './models/order-type.model';
+import {OrderStatus} from './models/order-status.model';
+import {UserType} from './models/user-type.model';
+import {User} from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +16,13 @@ import {DataBaseService} from './services/database.service';
 export class AppComponent {
   title = 'NEW-EPAM';
 
-  users = [];
-  userTypes = [];
+  users: User[] = [];
+  userTypes: UserType[] = [];
+  orderStatuses: OrderStatus[] = [];
+  orderTypes: OrderType[] = [];
+  orders: Order[] = [];
+  rules: Rule[] = [];
+  userTypeRules: UserTypeRule[] = [];
 
   constructor(private dataBaseService: DataBaseService) {
     this.getData();
@@ -20,5 +32,9 @@ export class AppComponent {
     this.users = await this.dataBaseService.getUsers();
     this.userTypes = await this.dataBaseService.getUserTypes();
     this.orderStatuses = await this.dataBaseService.getOrderStatuses();
+    this.orderTypes = await this.dataBaseService.getOrderTypes();
+    this.orders = await this.dataBaseService.getOrders();
+    this.rules = await this.dataBaseService.getRules();
+    this.userTypeRules = await this.dataBaseService.getUserTypeRules();
   }
 }
