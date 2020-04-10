5316218f0b4bf98d42a2dbdee307763d774a9a56
Nelmeris <kufaevartem@icloud.com>
2020-04-10 16:30:45 +0300

    Защита от пустого JSON

--- client/src/app/models/order/order-status.ts
+++ client/src/app/models/order/order-status.ts
@@ -3,6 +3,9 @@ export class OrderStatus {
     title: string;
 
     constructor(json) {
+        if (!json) {
+            return;
+        }
         this.id = json.id;
         this.title = json.title;
     }
