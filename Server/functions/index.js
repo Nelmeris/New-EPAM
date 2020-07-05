854c421991d9b58236875bea8eba7a8c4408ad75
Artem Kufaev <kufaevartem@icloud.com>
2020-07-05 22:39:50 +0400

    Реализован CRUD на GraphQL

--- Server/functions/index.js
+++ Server/functions/index.js
@@ -23,22 +23,87 @@ const typeDefs = gql`
     email: String!
     password: String!
     createdAt: String!
+    typeId: String!
   }
   type Query {
     users: [User] 
   }
+  type Mutation {
+      addUser(
+          name: String!,
+          surname: String!,
+          phoneNumber: String!,
+          email: String!,
+          password: String!,
+          typeId: String!,
+          createdAt: String!): User
+      updateUser(
+          id: ID!,
+          name: String!,
+          surname: String!,
+          phoneNumber: String!,
+          email: String!,
+          password: String!,
+          typeId: String!,
+          createdAt: String!): User
+      deleteUser(id: ID!): User
+  }
 `;
+
+function createUserFromArgs(args) {
+    return {
+        name: args.name,
+        surname: args.surname,
+        phoneNumber: args.phoneNumber,
+        email: args.email,
+        password: args.password,
+        createdAt: args.createdAt,
+        typeId: args.typeId
+    }
+}
+
+function parseSnapshot(snapshot) {
+    const object = snapshot.data();
+    object.id = snapshot.id;
+    return object;
+}
 
 const resolvers = {
     Query: {
         users: () =>
         db.collection('/users').get().then(snapshots =>
-            snapshots.docs.map(item => {
-                const user = item.data();
-                user.id = item.id;
-                return user;
-            })
+            snapshots.docs.map(snapshot => parseSnapshot(snapshot))
         )
+    },
+    Mutation: {
+        addUser: (parent, args) =>
+        db.collection('/users').add(
+            createUserFromArgs(args)
+        ).then(ref => 
+            ref.get().then(snapshot =>
+                parseSnapshot(snapshot)
+            )
+        ),
+
+        updateUser: (parent, args) =>
+        db.collection('/users').doc(args.id).set(
+            createUserFromArgs(args)
+        ).then(ref => 
+            db.collection('/users').get().then(snapshots =>
+                parseSnapshot(
+                    snapshots.docs
+                    .find(snapshot => snapshot.id === args.id)
+                )
+            )
+        ),
+        deleteUser: (parent, args) =>
+        db.collection('/users').get().then(snapshots => {
+            const userData = snapshots.docs
+            .find(snapshot => snapshot.id === args.id);
+            const user = parseSnapshot(userData);
+            db.collection('/users').doc(args.id).delete();
+            return user;
+        })
     }
 }
 
